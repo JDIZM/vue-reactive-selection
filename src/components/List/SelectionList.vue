cb39f23e62298e6418bb8c2e087d26a203c5bff5
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-03-26 17:53:36 +0000

    feat: selection list

---
+++ src/components/List/SelectionList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="list-group">
+    <slot />
+  </ul>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss">
+  .list-group {
+    list-style: none;
+    background-color: rgb(240, 223, 202);
+    padding: 1rem;
+  }
+</style>
---
+++ src/components/List/SelectionListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="list-item" @click="onClick($event)">
+    <slot />
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      clickDisabled: Boolean,
+    },
+    methods: {
+      onClick($event) {
+        if (this.clickDisabled) return;
+        this.$emit("click", $event);
+      },
+    },
+  };
+</script>
+
+<style lang="scss">
+  .list-item {
+    background-color: rgb(113, 156, 236);
+    padding: 1rem;
+    cursor: pointer;
+    &:hover {
+      background-color: rgb(68, 127, 236);
+    }
+  }
+</style>
