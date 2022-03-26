abbd6402c1306f8698bc8aee9fba372a31731b16
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-03-26 15:34:50 +0000

    feat: @ alias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 // https://vitejs.dev/config/
 const { createVuePlugin } = require("vite-plugin-vue2");
+import { join } from "path";
 
 module.exports = {
   plugins: [createVuePlugin(/*options*/)],
+  resolve: {
+    alias: {
+      "@": join(__dirname, "src"),
+    },
+  },
 };
