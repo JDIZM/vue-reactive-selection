040442611ff5708d9528d249a59369fdd7877d89
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-03-27 00:03:14 +0000

    feat: reactive form data

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,120 @@
 <template>
   <div id="app">
     <div class="container">
-      <h1>VUE REACTIVE DATA</h1>
-      <div v-if="loading" class="loading">Loading data...</div>
-      <div v-if="errorMsg" class="error">Error {{ errorMsg }}</div>
-      <div></div>
+      <nav>
+        <h1>VUE REACTIVE FORM DATA</h1>
+        <div v-if="loading" class="loading">Loading data...</div>
+        <div v-if="errorMsg" class="error">Error {{ errorMsg }}</div>
+      </nav>
+      <div v-show="!loading" class="list">
+        <p>select an item from the list, the form will display reactive data</p>
+        <selection-list :disabled="isModified">
+          <selection-list-item
+            v-for="(item, i) in clonedDataArray"
+            :key="i"
+            :click-disabled="isModified"
+            @click="selectListItem(item, i)"
+          >
+            <span class="icon">{{ item.cost }} ⭐️ </span> - {{ item.name }}
+          </selection-list-item>
+        </selection-list>
+      </div>
+      <div v-show="!loading" class="form">
+        <p>
+          watch for changes when the form data updates, disable selecting
+          another item if the data has been modified
+        </p>
+        <editable-form
+          v-if="showForm && formData && Object.entries(formData).length"
+          :list-index="selectedIndex"
+          :is-modified="isModified"
+          :form-data="formData"
+          @edited="isModified = true"
+          @submit="saveChanges"
+        />
+      </div>
     </div>
   </div>
 </template>
 |
 <script>
+  import sampleData from "@/data/sample.json";
+  import SelectionList from "@/components/List/SelectionList.vue";
+  import SelectionListItem from "@/components/List/SelectionListItem.vue";
+  import EditableForm from "@/components/Form/EditableForm.vue";
+  import Vue from "vue";
+
   export default {
     name: "App",
+    components: {
+      SelectionList,
+      SelectionListItem,
+      EditableForm,
+    },
     data() {
       return {
         loading: true,
-        errorMsg: "this is an error",
+        errorMsg: "",
+        sampleData,
+        isModified: false,
+        showForm: false,
+        selectedIndex: 0,
+        formData: null,
       };
+    },
+    computed: {
+      clonedDataArray() {
+        const cloned = [...this.sampleData.items];
+        return cloned;
+      },
+    },
+    async mounted() {
+      // example loading...
+      console.log("loading..");
+      await setTimeout(() => {
+        console.log("loaded data");
+        this.loading = false;
+        this.showForm = true;
+      }, 800);
+    },
+    methods: {
+      selectListItem(item, index) {
+        console.log(`selectListItem:`, index, item);
+        this.selectedIndex = index;
+        this.formData = item;
+      },
+      saveChanges(payload) {
+        // on button click
+        // versus auto timer
+        this.isModified = false;
+        console.log("saveChanges", payload);
+        const { index, updatedData } = payload;
+        // reactive
+        Vue.set(this.clonedDataArray, index, updatedData);
+      },
     },
   };
 </script>
 
-<style lang="css" scoped>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+<style lang="scss" scoped>
+  .loading {
+    background-color: var(--light);
+    color: var(--text-color-ld);
   }
-
+  .error {
+    background-color: var(--error);
+  }
+  nav {
+    padding-bottom: 1rem;
+  }
   .container {
-    padding: 0.5rem;
-    max-width: 770px;
-    margin: auto;
+    justify-content: flex-start;
   }
-
-  #option-select {
-    padding: 0.5rem;
+  .list {
+    width: 100%;
+    padding-bottom: 1rem;
+  }
+  .form {
+    padding-bottom: 1rem;
   }
 </style>
---
+++ src/components/Form/EditableForm.vue
@@ -0,0 +1,152 @@
+<template>
+  <v-contact-form
+    v-if="formData && Object.entries(formData).length"
+    :class="classes"
+    @submit="onSubmit"
+  >
+    <base-input
+      v-model="name"
+      type="text"
+      placeholder="empty"
+      :show-error="!isNameValid"
+      @input="onUpdate($event, 'name')"
+      @focus="onFocus($event, 'name')"
+    >
+      <template #label> name </template>
+      <template #icon> > </template>
+    </base-input>
+    <base-input
+      v-model.number="cost"
+      type="number"
+      placeholder="empty"
+      :show-error="!isCostValid"
+      @input="onUpdate($event, 'cost')"
+      @focus="onFocus($event, 'cost')"
+    >
+      <template #label> cost </template>
+      <template #icon>⭐️</template>
+    </base-input>
+  </v-contact-form>
+</template>
+
+<script>
+  import { VContactForm } from "@jdizm/vue-storybook";
+  import BaseInput from "@/components/BaseInput.vue";
+  import Vue from "vue";
+
+  export default {
+    components: {
+      BaseInput,
+      VContactForm,
+    },
+    props: {
+      isModified: Boolean,
+      listIndex: {
+        type: Number,
+        required: true,
+      },
+      formData: {
+        type: Object,
+        required: false,
+        default: null,
+      },
+    },
+    data() {
+      return {
+        name: "",
+        cost: 0,
+        // will retain the last updated items state
+        updatedData: {},
+      };
+    },
+    computed: {
+      classes() {
+        return {
+          "--modified": this.isModified,
+        };
+      },
+      computedUpdatedData() {
+        // method 1: using computed properties
+        const values = Object.values(this.updatedData);
+        console.log("computedUpdatedData", values);
+        return this.updatedData;
+      },
+      isNameValid() {
+        return typeof this.name === "string" && this.name.length;
+      },
+      isCostValid() {
+        return typeof this.cost === "number" && this.cost > 0;
+      },
+    },
+    watch: {
+      formData: {
+        // set data on first load with immediate
+        immediate: true,
+        handler(value) {
+          // set data when props change
+          const { name, cost } = value;
+          console.log("watch formData", value);
+          // v-model data without mutating props
+          this.name = name;
+          this.cost = cost;
+        },
+      },
+      updatedData: {
+        // method 2: using a deep watcher to watch changes
+        deep: true,
+        handler(newVal) {
+          // if updatedData is reset to empty
+          // it breaks watchers
+          // preserve the previous updated state
+          // console.log("watch updatedData old: ", oldVal);
+          console.log("watch updatedData new: ", newVal);
+          this.$emit("edited");
+        },
+      },
+    },
+    mounted() {
+      // mounted triggers after immediate watcher
+      console.log("mounted", this.formData);
+    },
+    methods: {
+      onSubmit() {
+        if (!this.isNameValid) return;
+        if (!this.isCostValid) return;
+        if (!this.isModified) return;
+
+        const payload = {
+          updatedData: { ...this.formData, ...this.updatedData },
+          index: this.listIndex,
+        };
+        console.log("submit", payload);
+        this.$emit("submit", payload);
+        // do not reset state of updatedData
+        // this breaks watchers, state and submit.
+        // this.updatedData = {};
+      },
+      onUpdate(data, key) {
+        console.log("onUpdate", data, key);
+        // number converts to string
+        // Vue.set(this.updatedData, key, data); // we lose the type this way
+        // maintain reactive state
+        Vue.set(this.updatedData, "name", this.name);
+        Vue.set(this.updatedData, "cost", this.cost);
+      },
+      // eslint-disable-next-line no-unused-vars
+      onFocus($event, key) {
+        // console.log("focus", key, $event);
+      },
+    },
+  };
+</script>
+
+<style lang="scss">
+  .form {
+    width: 100%;
+    margin: 0 auto;
+    padding: 1rem;
+    &.--modified {
+      border: 2px dashed orangered;
+    }
+  }
+</style>
